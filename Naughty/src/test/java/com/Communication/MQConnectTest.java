cfc2a7ff2e6b38bf856b95b62f50a9f47d0795ca
linsimiao <linsm8968@126.com>
2016-03-30 01:49:50 +0800

    add some test

--- Naughty/src/test/java/com/Communication/MQConnectTest.java
+++ Naughty/src/test/java/com/Communication/MQConnectTest.java
@@ -1,14 +1,20 @@
-package Client;
+package com.Communication;
+
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-public class TestRunner {
+/**
+ * Created by fowafolo
+ * Date: 16/3/30
+ * Time: 01:37
+ */
+public class MQConnectTest {
     public static void main(String[] args) {
-        Result result = JUnitCore.runClasses(WriteLogTest.class);
+        Result result = JUnitCore.runClasses(MQConnectTest.class);
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
         }
         System.out.println(result.wasSuccessful());
     }
-} 
+}
---
+++ Naughty/src/test/java/com/Util/WriteLogTest.java
@@ -0,0 +1,96 @@
+package com.Util;
+
+import com.Util.WriteLog;
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Date;
+
+import static junit.framework.Assert.assertEquals;
+
+/** 
+* WriteLog Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>���� 29, 2016</pre> 
+* @version 1.0 
+*/ 
+public class WriteLogTest { 
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+    *
+    * Method: write(String fileName, String content)
+    *
+    */
+    @Ignore
+    public void testWrite() throws Exception {
+    //TODO: Test goes here...
+       //向文件输入一行数据，测试输入数据与文件最后一行数据是否相同；
+        String txt = "testMeAgain";
+        FileReader fr = new FileReader("testWrite.txt");
+        WriteLog.write("testWrite.txt", txt);
+        String lastLine= null;
+
+        BufferedReader br = new BufferedReader(fr);
+        String str = null;
+        while ((str = br.readLine()) != null) {
+            lastLine = str;
+        }
+        br.close();
+        fr.close();
+        assertEquals(txt,lastLine);
+
+     // 打开一个随机访问文件流，按读写方式
+
+    }
+
+    /**
+    *
+    * Method: writeLoginForClient(String ClientName, boolean result)
+    *
+    */
+    @Test
+    public void testWriteLoginForClient() throws Exception {
+    //TODO: Test goes here...
+        Date date = new Date();
+        String ClientName = "abc";
+        boolean result = true;
+        WriteLog.writeLoginForClient(ClientName, result);
+
+        FileReader fr = new FileReader("Log.txt");
+        String lastLine= null;
+        BufferedReader br = new BufferedReader(fr);
+        String str = null;
+        while ((str = br.readLine()) != null) {
+            lastLine = str;
+        }
+        br.close();
+        fr.close();
+        assertEquals("Log.txt", "Client: " + ClientName + " login " + result + " at " + date,lastLine);
+
+    }
+
+    /**
+    *
+    * Method: createNewFile(String name)
+    *
+    */
+    @Test
+    public void testCreateNewFile() throws Exception {
+    //TODO: Test goes here...
+    }
+
+
+} 
