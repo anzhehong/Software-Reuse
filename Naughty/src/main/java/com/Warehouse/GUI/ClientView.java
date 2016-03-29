7c8f7e28c2fc695c253799b698afeb7f71b2b8a9
1352871 <243069980@qq.com>
2016-03-29 17:08:12 +0800

    客户端界面基本完成：增加了滚动条，解决了编辑区换行相对位置移动的问题

--- Naughty/src/main/java/com/Warehouse/GUI/ClientView.java
+++ Naughty/src/main/java/com/Warehouse/GUI/ClientView.java
@@ -1,19 +1,25 @@
 package com.Warehouse.GUI;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 /**
  * Created by Kang Huilin on 2016/3/27.
  */
 public class ClientView extends JFrame {
+    public JPanel contentPane;
+    public JScrollPane scrollPane;
+
 
     public JTextArea MessageShow;//消息展示区
     public JTextArea MessageEdit;//消息编辑区
-    public JTextArea SplitLine;  //分割线
-    public JTextArea BlankArea;  //分割线
+    public  JTextArea BlankArea;
     public JButton ConfirmButton; //发送键
     public JButton CloseButton;//关闭键
+    //public JTextArea SplitLine;  //分割线
+
+
     public static void main(String args[] )
     {
         ClientView clientView=new ClientView();
@@ -22,68 +28,77 @@ public class ClientView extends JFrame {
     public ClientView()
     {
         this.init();
-        this.setSize(600,600);
+        this.setSize(750,600);
         this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 
     public void init() {
 
+        contentPane=new JPanel();
+        contentPane.setBorder(new EmptyBorder(30,15,15,15));
         GridBagLayout layout = new GridBagLayout();
-        this.setLayout(layout);
+        contentPane.setLayout(layout);
+        this.setContentPane(contentPane);
+        scrollPane=new JScrollPane();
 
-        //MessageShow 面板
+        layout.columnWidths = new int[]{400, 100, 100, 0};
+        layout.columnWeights = new double[]{0, 0, 0, 1.0E-4};
+        layout.rowHeights = new int[]{250, 1, 225, 24, 0};
+        layout.rowWeights = new double[]{0, 0, 0, 0, 1.0E-4};
+
+       //MessageShow 面板
         MessageShow = new JTextArea();
         MessageShow.setEnabled(false);
-        this.add(MessageShow, new GBC(0, 0, 3, 1).
+        this.add(MessageShow,
+                new GBC(0, 0, 6, 1).
                 setFill(GBC.BOTH).
-                setIpad(600, 300).
-                setWeight(1, 1));
-
-        //SplitLine 分割线
-        SplitLine = new JTextArea();
-        SplitLine.setEnabled(false);
-        SplitLine.setBackground(Color.lightGray);
-        this.add(SplitLine, new GBC(0, 1, 3, 1).
-                setFill(GBC.BOTH).
-                setIpad(600, 1).
-                setWeight(1, 0));
-
+                setWeight(1, 250));
 
         //MessageEdit
         MessageEdit = new JTextArea();
         MessageEdit.setLineWrap(true);
-        this.add(MessageEdit, new GBC(0, 2, 3, 1).
-                setFill(GBC.BOTH).
-                setIpad(600,249 ).
-                setWeight(1, 1));
+        scrollPane.setVerticalScrollBarPolicy( JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
+        this.add(scrollPane,
+                new GBC(0, 2, 6, 1).
+                setFill(GBC.BOTH).
+                setWeight(1, 225));
+        scrollPane.setViewportView(MessageEdit);
 
         //BlankArea 面板
         BlankArea = new JTextArea();
-        BlankArea.setBackground(Color.white);
         BlankArea.setEnabled(false);
-        this.add(BlankArea, new GBC(0, 1, 1, 1).
+        BlankArea.setBackground(new Color(235,235,235));
+        this.add(BlankArea,
+                new GBC(0, 3, 1, 1).
                 setFill(GBC.BOTH).
-                setIpad(400, 20).
-                setWeight(1, 1));
-
+                setWeight(1, 24));
 
         CloseButton = new JButton("exit");
-        this.add(CloseButton, new GBC(1, 3, 1, 1).
-                setFill(GBC.NONE).
-                setIpad(100, 15).
-                setWeight(1, 0));
-
+        this.add(CloseButton,
+                new GBC(1, 3, 1, 1).
+                setFill(GBC.BOTH).
+                setWeight(0, 24).setInsets(10,5,5,3));
 
         ConfirmButton=new JButton("send");
-        this.add(ConfirmButton, new GBC(2, 3, 1, 1).
-                setFill(GBC.NONE).
-                setIpad(100, 15).
-                setWeight(1, 0));
+        this.add(ConfirmButton,
+                new GBC(2, 3, 1, 1).
+                setFill(GBC.BOTH).
+                setWeight(0, 24).setInsets(10,3,5,5));
+
+
+        /*
+        //SplitLine 分割线
+        SplitLine = new JTextArea();
+        SplitLine.setBackground(Color.PINK);
+        SplitLine.setEnabled(false);
+        this.add(SplitLine,
+                new GBC(0, 1, 6, 1).
+                setFill(GBC.BOTH).
+                setWeight(1, 1));
+        */
+
     }
-
-
-
-
 }
