3f01ba140a81a7deba79c69ee04318e9ed248157
kobpko <zhangjiaqi121@126.com>
2016-04-17 22:32:21 +0800

    更新PM

--- NaughtyProject/PM/src/main/java/PMManager.java
+++ NaughtyProject/PM/src/main/java/PMManager.java
@@ -1,11 +1,54 @@
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by MSI on 2016/3/28.
  */
 public class PMManager {
+    public  Timer timer;
+    private static int inValidLoginCount;
+    private static int validLoginCount;
+    private static String filepath;
+    private long delaySecond;
+
+    public PMManager(String _filePath,int _delayMinute) {
+        filepath = _filePath;
+        delaySecond = _delayMinute * 60000;
+    }
+
+    public void LogSuccess(){
+        validLoginCount++;
+    }
+
+    public void LogFail(){
+        inValidLoginCount++;
+    }
+
+
+    public void startRecord( ){
+        timer = new Timer();
+        timer.schedule(new WriteLoginTask(), delaySecond, delaySecond);
+
+    }
+
+    static class WriteLoginTask extends TimerTask
+    {
+        public void run() {
+            //TODO: 把validLogin和invalidLogin记录到文件中
+            String currentMins = new SimpleDateFormat("yyyy_MM_dd HH mm ss").format(Calendar.getInstance().getTime());
+            String fileName = "LogInRecordAt" +currentMins;
+            System.out.println(filepath+fileName+ ".txt");
+            PMManager.Write(fileName + ".txt", currentMins + "\tValid Login Count: " + validLoginCount + "\tInvalid Login Count: " + inValidLoginCount, filepath);
+            inValidLoginCount = 0;
+            validLoginCount = 0;
+        }
+    }
+
     /**
      * 写文件
      * @param fileName 文件路径
@@ -26,18 +69,22 @@ public class PMManager {
         }
     }
 
-    /**
-     * 记录登录记录，包括时间、client使用的username、是否成功
-     * @param ClientName
-     * @param result
-     */
-    public static void WriteLoginForClient(String ClientName , boolean result, String outPath){
-        Date date = new Date();
-        Write("Log.txt","Client: " + ClientName + " login " + result + " at " + date, outPath);
-    }
+//    /**
+//     * 记录登录记录，包括时间、client使用的username、是否成功
+//     * @param ClientName
+//     * @param result
+//     */
+//    public static void WriteLoginForClient(String ClientName , boolean result, String outPath){
+//        Date date = new Date();
+//        Write("Log.txt","Client: " + ClientName + " login " + result + " at " + date, outPath);
+//    }
 
-    public static void createNewFile(String name){
-
+    public static void main(String[] args) {
+        PMManager pmManager = new PMManager("C:\\Users\\MSI\\Desktop\\",1);
+        pmManager.startRecord();
+        pmManager.LogSuccess();
+        pmManager.LogSuccess();
+        pmManager.LogFail();
     }
 
 
---
+++ NaughtyProject/PM/src/main/java/PrivatePM.java
@@ -0,0 +1,60 @@
+/**
+ * Created by MSI on 2016/4/13.
+ */
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+
+public class PrivatePM{
+
+    private List<String> stringlist;
+
+    public PrivatePM() {
+        this.stringlist = new ArrayList<String>();
+    }
+
+
+    public void addkey(String key){
+        stringlist.add(key);
+    }
+
+    public void Write(String fileName, String content, String outPath){
+        try {
+            // 打开一个随机访问文件流，按读写方式
+            RandomAccessFile randomFile = new RandomAccessFile(outPath + fileName, "rw");
+            // 文件长度，字节数
+            long fileLength = randomFile.length();
+            // 将写文件指针移到文件尾。
+            randomFile.seek(fileLength);
+            Date now = new Date();
+            randomFile.writeBytes(stringtransfer(content) + "\ttime:\t" + now + "\r\n");
+            randomFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    private String stringtransfer(String input){
+        String[] sourceStrArray=input.split(",",stringlist.size());
+        String output= "";
+        for(int i =0;i<sourceStrArray.length;i++){
+            output = output + stringlist.get(i) + ":" + sourceStrArray[i] + "\t";
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        PrivatePM privatePM = new PrivatePM();
+        privatePM.addkey("name");
+        privatePM.addkey("value");
+        privatePM.addkey("grade");
+        String a = privatePM.stringtransfer("qinbo,shabi,10");
+        System.out.print(a);
+    }
+}
