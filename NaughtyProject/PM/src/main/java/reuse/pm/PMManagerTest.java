91d75f71a73ba57a3cfe4a5dd684ee3fda53c0fc
1352871 <243069980@qq.com>
2016-05-25 12:37:52 +0800

    DebugLog+ErrorLog+Zip测试代码

---
+++ NaughtyProject/PM/src/main/java/reuse/pm/PMManagerTest.java
@@ -0,0 +1,110 @@
+package reuse.pm;
+
+import org.junit.Test;
+import reuse.cm.ReadJson;
+
+import java.io.*;
+
+import static org.junit.Assert.*;
+import static reuse.pm.PMManager.jsonPath;
+
+/**
+ * Created by acer88 on 2016/5/25.
+ */
+public class PMManagerTest {
+
+    @Test
+    public void errorLog() throws Exception {
+        String errorPath = new ReadJson(jsonPath).getStringConfig("ErrorLogPath") + File.separator;
+       // System.out.println(errorPath);
+        String errorName = "ErrorLog.txt";
+        String errorMsg="errorTest";
+        String className=this.getClass().getName();
+        int lineNumber=100;
+        PMManager.ErrorLog(errorName, errorMsg, className, lineNumber, errorPath);
+        File file=new File(errorPath + "ErrorLog.txt");
+        String ErrorLog = readLastLine(file);
+        System.out.println(ErrorLog);
+        String ErrorLog_=className + ": " + lineNumber + "  " + errorMsg+"\r\n";
+        System.out.println(ErrorLog_);
+
+     /*  if(ErrorLog.equals(ErrorLog_))
+       {
+           System.out.println("errorLog() successfully！");
+       }
+       else
+       {
+           System.out.println("errorlog() failed!");
+       }
+       */
+    }
+
+
+    @Test
+    public void debugLog() throws Exception {
+        String DebugPath = new ReadJson(jsonPath).getStringConfig("DebugLogPath") + File.separator;
+        // System.out.println(errorPath);
+        String DebugName = "DebugLog.txt";
+        String DebugMsg="DebugTest";
+        String className=this.getClass().getName();
+        int lineNumber=99;
+        PMManager.ErrorLog(DebugName, DebugMsg,className, lineNumber,DebugPath);
+        File file=new File(DebugPath + "DebugLog.txt");
+        String DebugLog = readLastLine(file);
+        System.out.println(DebugLog);
+        String DebugLog_=className + ": " + lineNumber + "  " + DebugMsg+"\r\n";
+        System.out.println(DebugLog_);
+
+     /* if(DebugLog.equals(DebugLog_))
+       {
+           System.out.println("DebugLog() successfully！");
+       }
+       else
+       {
+           System.out.println("Debuglog() failed!");
+       }
+       */
+    }
+
+
+
+    public static String readLastLine(File file) throws IOException {
+        if (!file.exists() || file.isDirectory() || !file.canRead()) {
+            return null;
+        }
+        RandomAccessFile raf = null;
+        try {
+            raf = new RandomAccessFile(file, "r");
+            long len = raf.length();
+            if (len == 0L) {
+                return "";
+            } else {
+                long pos = len - 1;
+                while (pos > 0) {
+                    pos--;
+                    raf.seek(pos);
+                    if (raf.readByte() == '\n') {
+                        break;
+                    }
+                }
+                if (pos == 0) {
+                    raf.seek(0);
+                }
+                byte[] bytes = new byte[(int) (len - pos)];
+                raf.read(bytes);
+                return new String(bytes);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (raf != null) {
+                try {
+                    raf.close();
+                } catch (Exception ea) {
+                    ea.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+}
---
+++ NaughtyProject/Utility/src/test/java/ZipTest.java
@@ -0,0 +1,48 @@
+
+import org.junit.Test;
+import reuse.utility.Zip;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by acer88 on 2016/5/24.
+ */
+public class ZipTest {
+
+    @Test
+    public void zip() throws Exception {
+
+    }
+
+    @Test
+    public void unZip() throws Exception {
+
+    }
+
+    @Test
+    public void zipDaily() throws Exception {
+        Zip.zipDaily();
+
+    }
+
+    @Test
+    public void logDaily() throws Exception {
+
+    }
+
+    @Test
+    public void logWeekly() throws Exception {
+
+    }
+
+    @Test
+    public void zipWeekly() throws Exception {
+
+    }
+
+    @Test
+    public void getFileLength() throws Exception {
+
+    }
+
+}
